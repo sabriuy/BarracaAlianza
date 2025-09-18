f000d9a99cc8cf2807bc8fcc27fc8635e2d7c205
Sabrina <sabrinaalejandra1992@gmail.com>
2025-09-18 07:42:42 -0300

    Update comprasRoutes.js

--- Backendbarraca/routes/comprasRoutes.js
+++ Backendbarraca/routes/comprasRoutes.js
@@ -3,10 +3,6 @@ const express = require('express');
 const router = express.Router();
 const comprasController = require('../controllers/comprasController');
 
-// Aquí puedes proteger con authMiddleware si ya lo importas en app.js
-// router.use(require('../middleware/auth'));
-
 router.post('/', comprasController.createCompra);
-// más endpoints: GET, PUT, etc.
 
 module.exports = router;
